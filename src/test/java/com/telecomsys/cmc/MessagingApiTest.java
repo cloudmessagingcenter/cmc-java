325fa8c59e0f01a304f1ce5295eef9ecf6217f14
saijee <saijee@gmail.com>
2015-09-30 15:03:07 -0400

    Adding test cases for messaging.

--- src/test/java/com/telecomsys/cmc/MessagingApiTest.java
+++ src/test/java/com/telecomsys/cmc/MessagingApiTest.java
@@ -129,4 +129,42 @@ public class MessagingApiTest {
         assertEquals(requests.get(0).getBodyAsString(), "{\"sendmessage\":{\"message\":\"Test message\",\"to\":[\"4102804827\"],\"from\":\"scsrest\"}}");
     }    
     
+    @Test
+    public void sendMessagesMultipleDestinations() throws CMCException {
+        stubFor(post(urlEqualTo("/messages"))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("{\"response\":{\"status\":\"success\",\"notifications\":{\"to\":[\"4102804827\",\"4102804828\"],\"from\":\"scsrest\",\"trackinginformation\":[{\"destination\":\"4102804827\",\"messagestatus\":\"Message Accepted\",\"messageID\":\"GW1_AVvciGlHRM32pw0Q\",\"messagetext\":\"Test message\"},{\"destination\":\"4102804828\",\"messagestatus\":\"Message Accepted\",\"messageID\":\"GW1_AVvciGlHRM32Rg0O\",\"messagetext\":\"Test message\"}]}}}")));        
+        
+        List<String> destinations = new ArrayList<String>();
+        destinations.add("4102804827");
+        destinations.add("4102804828"); 
+        Message message = new Message(destinations, REST_CONNECTION_KEYWORD, "Test message");
+        message.setNotifyURL("http://customer.com/notifications");
+        message.setReplyExpiry(60);
+        HttpResponseWrapper<NotificationsResponse> response = messagingApi.sendMessage(message);
+        
+        // Verify the response
+        assertEquals(response.getHttpStatusCode(), 200);
+        Notifications notifications = response.getResponseBody().getNotifications();
+        assertEquals(notifications.getFromAddress(),"scsrest");
+        assertEquals(notifications.getTo().size(),2);
+        assertEquals(notifications.getTo().get(0),"4102804827");
+        assertEquals(notifications.getTo().get(1),"4102804828");
+        List<TrackingInformation> trackingInformation = notifications.getTrackingInformation();
+        assertEquals(trackingInformation.size(),2);
+        assertEquals(trackingInformation.get(0).getDestination(),"4102804827");
+        assertEquals(trackingInformation.get(0).getMessageID(),"GW1_AVvciGlHRM32pw0Q");
+        assertEquals(trackingInformation.get(0).getMessageText(),"Test message");
+        assertEquals(trackingInformation.get(1).getDestination(),"4102804828");
+        assertEquals(trackingInformation.get(1).getMessageID(),"GW1_AVvciGlHRM32Rg0O");
+        assertEquals(trackingInformation.get(1).getMessageText(),"Test message");
+
+        // Verify the request
+        List<LoggedRequest> requests = findAll(postRequestedFor(urlMatching("/messages")));
+        assertEquals(requests.size(), 1);
+        assertEquals(requests.get(0).getBodyAsString(), "{\"sendmessage\":{\"message\":\"Test message\",\"to\":[\"4102804827\",\"4102804828\"],\"notifyURL\":\"http://customer.com/notifications\",\"replyexpiry\":60,\"from\":\"scsrest\"}}");
+    }    
+    
 }
