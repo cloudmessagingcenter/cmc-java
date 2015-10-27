590e773b8f11cb78f3d4415e2b70d50193f68864
sultanai <isultana21@gmail.com>
2015-10-27 16:36:56 -0400

    Adding group response details.

--- src/main/java/com/telecomsys/cmc/response/GroupResponse.java
+++ src/main/java/com/telecomsys/cmc/response/GroupResponse.java
@@ -2,6 +2,7 @@ package com.telecomsys.cmc.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.telecomsys.cmc.model.Group;
 
 /**
  * CMC REST group response for group retrieval.
@@ -9,4 +10,30 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 @JsonRootName(value = "response")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GroupResponse extends RestResponse {
+
+    /**
+     * Group details.
+     */
+    private Group group;
+
+    /**
+     * @return the group
+     */
+    public Group getGroup() {
+        return group;
+    }
+
+    /**
+     * @param group the group to set
+     */
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Group: '").append(group).append("'");
+        return sb.toString();
+    }
 }
